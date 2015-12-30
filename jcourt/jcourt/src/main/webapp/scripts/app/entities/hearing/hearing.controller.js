0e74cb8f7a7d62b998fbcaf4fb1fcb878d3368b9
greyfox33 <robb@oakts.com>
2015-12-29 17:56:58 -0800

    updates starting to push to jhip3

--- jcourt/jcourt/src/main/webapp/scripts/app/entities/hearing/hearing.controller.js
+++ jcourt/jcourt/src/main/webapp/scripts/app/entities/hearing/hearing.controller.js
@@ -25,7 +25,26 @@ angular.module('jcourt2App')
                     $scope.clear();
                 });
         };
+        // (RMT-12/29/15) sending the post request goes here
+        $scope.sendHearing = function (id) {
+        	//build the $http resource
+        	//set a temp hearing equal to the current hearing
+        	//set the transfer hearing values to match only the subset needed (e.g. leave out Judge, and don't set caseworker
+        	//build the http command 
+        	// leave a note  here to build the http command using applicatio.yml values, not hard coded
+        	/*
+            .factory('dataFactory', ['$http', function($http) {
 
+                var urlBase = 'http://127.0.0.1:8050/api/hearing';
+                var dataFactory = {};
+                -- insert http command, like bearer token here
+
+                dataFactory.insertCustomer = function (cust) {
+                    return $http.post(urlBase, cust);
+                };
+             */
+        }
+        
         $scope.refresh = function () {
             $scope.loadAll();
             $scope.clear();
